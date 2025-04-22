1968414703698ec3e21a94a1c0e244b6e4c5bbd7
Artem <artem.23.cz@gmail.com>
2025-04-22 19:29:10 +0900

    last commit

--- script.js
+++ script.js
@@ -1,31 +1,46 @@
- const button = document.querySelector('.button');
- const list =  document.querySelector('.users');
- const message = document.querySelector('.message');
+ const button = document.getElementById('button');
+ const list =  document.getElementById('users');
+ const message = document.getElementById('message');
  
  function throttle (func, delay) {
+    let isThrottled = false;
 
     function wrapper () {
+        if (isThrottled) return;
 
+        func.apply(this, arguments);
+
+        isThrottled = true;
+        
+        setTimeout(function() {
+            isThrottled = false;
+        }, delay)
     }
     return wrapper
  }
 
- button.addEventListener('onclick')
- function fetchData() {
-    message.textContent = "Fetching users... Please wait."
-    setTimeout(async () => {
-        try {
-            const response = await fetch ('https://jsonplaceholder.typicode.com/users');
-            const data = response.json();
-            console.log(data);
-            
-             
-            }
-        catch(e) {
 
-        }
-    }, 2000);
+ async function fetchData() {
+    list.innerHTML = ' ';
+    message.textContent = "Fetching users... Please wait.";
+    try {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+
+    const data = await response.json();
+    data.forEach(user => {
+        const li = document.createElement('li');
+        li.textContent = user.name;
+        list.appendChild(li);
+    });
+    } 
+    catch(e) {
+        console.error("Fetch error", e);
+        message.textContent = "Failed to load users";
+        message.style.color = 'red'
+    }
 
 }
+button.addEventListener('click', throttle(fetchData, 2000))
 
 console.log('Hello');
